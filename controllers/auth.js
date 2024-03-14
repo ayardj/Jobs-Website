8f21235435fe5c71669afed6a2de79e2437e8c56
ayardj <youtay08@gmail.com>
2024-03-14 19:58:57 +0100

    first modification

--- controllers/auth.js
+++ controllers/auth.js
@@ -6,9 +6,9 @@ const { BadRequestError, UnauthenticatedError } = require('../errors')
 
 const register = async (req,res) => {
     const{ name, email, password} = req.body
-    if(!name || !email || !password){
-        throw new BadRequestError('Please provide name,email and password')
-    }
+    //if(!name || !email || !password){
+      //  throw new BadRequestError('Please provide name,email and password')
+    //}
     
     const user = await User.create({...req.body})
     const token = user.createJWT()
--- controllers/jobs.js
+++ controllers/jobs.js
@@ -23,17 +23,42 @@ const getJob = async (req,res) => {
 
 const createJob = async (req,res) => {
     req.body.createdBy = req.user.userId
-    const job = await job.create(req.body)
+    const job = await Job.create(req.body)
     res.status(StatusCodes.CREATED).json({job})
     
 }
 
 const updateJob = async (req,res) => {
-    res.send('update job')
+    const {
+        body:{company,position},
+        user:{userId},
+        params:{id:jobId}
+    } = req
+
+    if(company === '' || position === ''){
+        throw new BadRequestError('Company or Position fields cannot be empty')
+    }
+    const job = await Job.findByIdAndUpdate({_id:jobId,createdBy:userId},req.body,{new:true,runValidators:true})
+    if(!job){
+        throw new NotFoundError('No job with '+jobId)
+    }
+    res.status(StatusCodes.OK).json({job})
 }
 
 const deleteJob = async (req,res) => {
-    res.send('delete job')
+    const {
+        body:{company,position},
+        user:{userId},
+        params:{id:jobId}
+    } = req
+    const job = await Job.findByIdAndRemove({
+        _id:jobId,
+        createdBy:userId
+    })
+    if(!job){
+        throw new NotFoundError('No job with '+jobId)
+    }
+    res.status(StatusCodes.OK).send()
 }
 
 
